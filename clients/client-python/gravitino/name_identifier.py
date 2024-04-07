ffdd9bc7a99ecbeb3897bccf5bd45a457a17b1ba
xunliu <xun@datastrato.com>
2024-04-07 18:48:30 +0800

    Add commonts

--- clients/client-python/gravitino/name_identifier.py
+++ clients/client-python/gravitino/name_identifier.py
@@ -4,10 +4,10 @@ from gravitino.namespace import Namespace
 
 class NameIdentifier:
     """
-     * A name identifier is a sequence of names separated by dots. It's used to identify a metalake, a
-     * catalog, a schema or a table. For example, "metalake1" can represent a metalake,
-     * "metalake1.catalog1" can represent a catalog, "metalake1.catalog1.schema1" can represent a
-     * schema.
+    A name identifier is a sequence of names separated by dots. It's used to identify a metalake, a
+    catalog, a schema or a table. For example, "metalake1" can represent a metalake,
+    "metalake1.catalog1" can represent a catalog, "metalake1.catalog1.schema1" can represent a
+    schema.
     """
 
     DOT = '.'
@@ -25,6 +25,12 @@ class NameIdentifier:
 
     @staticmethod
     def of(*names: str) -> 'NameIdentifier':
+        """
+        Create the NameIdentifier with the given {@link Namespace} and name.
+        @param namespace The namespace of the identifier
+        @param name The name of the identifier
+        @return The created NameIdentifier
+        """
         NameIdentifier.check(names is not None, "Cannot create a NameIdentifier with null names")
         NameIdentifier.check(len(names) != 0, "Cannot create a NameIdentifier with no names")
 
@@ -32,10 +38,21 @@ class NameIdentifier:
 
     @staticmethod
     def of_namespace(namespace: Namespace, name: str) -> 'NameIdentifier':
+        """
+        Create the metalake NameIdentifier with the given name.
+        @param metalake The metalake name
+        @return The created metalake NameIdentifier
+        """
         return NameIdentifier(namespace, name)
 
     @staticmethod
     def of_metalake(metalake: str) -> 'NameIdentifier':
+        """
+        Create the catalog NameIdentifier with the given metalake and catalog name.
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @return The created catalog NameIdentifier
+        """
         return NameIdentifier.of(metalake)
 
     @staticmethod
@@ -44,64 +61,143 @@ class NameIdentifier:
 
     @staticmethod
     def of_schema(metalake: str, catalog: str, schema: str) -> 'NameIdentifier':
+        """
+        Create the schema NameIdentifier with the given metalake, catalog and schema name.
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @param schema The schema name
+        @return The created schema NameIdentifier
+        """
         return NameIdentifier.of(metalake, catalog, schema)
 
     @staticmethod
     def of_table(metalake: str, catalog: str, schema: str, table: str) -> 'NameIdentifier':
+        """
+        Create the table NameIdentifier with the given metalake, catalog, schema and table name.
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @param schema The schema name
+        @param table The table name
+        @return The created table NameIdentifier
+        """
         return NameIdentifier.of(metalake, catalog, schema, table)
 
     @staticmethod
     def of_fileset(metalake: str, catalog: str, schema: str, fileset: str) -> 'NameIdentifier':
+        """
+        Create the fileset NameIdentifier with the given metalake, catalog, schema and fileset name.
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @param schema The schema name
+        @param fileset The fileset name
+        @return The created fileset NameIdentifier
+        """
         return NameIdentifier.of(metalake, catalog, schema, fileset)
 
     @staticmethod
     def of_topic(metalake: str, catalog: str, schema: str, topic: str) -> 'NameIdentifier':
+        """
+        Create the topic NameIdentifier with the given metalake, catalog, schema and topic
+        name.
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @param schema The schema name
+        @param topic The topic name
+        @return The created topic NameIdentifier
+        """
         return NameIdentifier.of(metalake, catalog, schema, topic)
 
     @staticmethod
     def check_metalake(ident: 'NameIdentifier') -> None:
+        """
+        Check the given NameIdentifier is a metalake identifier. Throw an {@link
+        IllegalNameIdentifierException} if it's not.
+        @param ident The metalake NameIdentifier to check.
+        """
         NameIdentifier.check(ident is not None, "Metalake identifier must not be null")
         Namespace.check_metalake(ident.namespace)
 
     @staticmethod
     def check_catalog(ident: 'NameIdentifier') -> None:
+        """
+        Check the given NameIdentifier is a catalog identifier. Throw an {@link
+        IllegalNameIdentifierException} if it's not.
+        @param ident The catalog NameIdentifier to check.
+        """
         NameIdentifier.check(ident is None, "Catalog identifier must not be null")
         Namespace.check_catalog(ident.namespace)
 
     @staticmethod
     def check_schema(ident: 'NameIdentifier') -> None:
+        """
+        Check the given NameIdentifier is a schema identifier. Throw an {@link
+        IllegalNameIdentifierException} if it's not.
+        @param ident The schema NameIdentifier to check.
+        """
         NameIdentifier.check(ident is not None, "Schema identifier must not be null")
         Namespace.check_schema(ident.namespace)
 
     @staticmethod
     def check_table(ident: 'NameIdentifier') -> None:
+        """
+        Check the given NameIdentifier is a table identifier. Throw an {@link
+        IllegalNameIdentifierException} if it's not.
+        @param ident The table NameIdentifier to check.
+        """
         NameIdentifier.check(ident is not None, "Table identifier must not be null")
         Namespace.check_table(ident.namespace)
 
     @staticmethod
     def check_fileset(ident: 'NameIdentifier') -> None:
+        """
+        Check the given NameIdentifier is a fileset identifier. Throw an {@link
+        IllegalNameIdentifierException} if it's not.
+        @param ident The fileset NameIdentifier to check.
+        """
         NameIdentifier.check(ident is not None, "Fileset identifier must not be null")
         Namespace.check_fileset(ident.namespace)
 
     @staticmethod
     def check_topic(ident: 'NameIdentifier') -> None:
+        """
+        Check the given NameIdentifier is a topic identifier. Throw an {@link
+        IllegalNameIdentifierException} if it's not.
+        @param ident The topic NameIdentifier to check.
+        """
         NameIdentifier.check(ident is not None, "Topic identifier must not be null")
         Namespace.check_topic(ident.namespace)
 
     @staticmethod
     def parse(identifier: str) -> 'NameIdentifier':
+        """
+        Create a NameIdentifier from the given identifier string.
+        @param identifier The identifier string
+        @return The created NameIdentifier
+        """
         NameIdentifier.check(identifier is not None and identifier != '', "Cannot parse a null or empty identifier")
 
         parts = identifier.split(NameIdentifier.DOT)
         return NameIdentifier.of(*parts)
 
     def has_namespace(self):
+        """
+        Check if the NameIdentifier has a namespace.
+        @return True if the NameIdentifier has a namespace, false otherwise.
+        """
         return not self.namespace.is_empty()
 
     def get_namespace(self):
+        """
+        Get the namespace of the NameIdentifier.
+        @return The namespace of the NameIdentifier.
+        """
         return self.namespace
 
     def get_name(self):
+        """
+        Get the name of the NameIdentifier.
+        @return The name of the NameIdentifier.
+        """
         return self.name
 
     def __eq__(self, other):
@@ -120,5 +216,11 @@ class NameIdentifier:
 
     @staticmethod
     def check(condition, message, *args):
+        """
+        Check the given condition is true. Throw an {@link IllegalNameIdentifierException} if it's not.
+        @param condition The condition to check.
+        @param message The message to throw.
+        @param args The arguments to the message.
+        """
         if not condition:
             raise IllegalNameIdentifierException(message.format(*args))
--- clients/client-python/gravitino/namespace.py
+++ clients/client-python/gravitino/namespace.py
@@ -2,17 +2,14 @@
 Copyright 2024 Datastrato Pvt Ltd.
 This software is licensed under the Apache License version 2.
 """
-import string
-
-from gravitino.exceptions.illegal_namespace_exception import IllegalNamespaceException
 from typing import List
 
 
 class Namespace:
     """
-     * A namespace is a sequence of levels separated by dots. It's used to identify a metalake, a
-     * catalog or a schema. For example, "metalake1", "metalake1.catalog1" and
-     * "metalake1.catalog1.schema1" are all valid namespaces.
+    A namespace is a sequence of levels separated by dots. It's used to identify a metalake, a
+    catalog or a schema. For example, "metalake1", "metalake1.catalog1" and
+    "metalake1.catalog1.schema1" are all valid namespaces.
     """
 
     EMPTY = None
@@ -23,10 +20,21 @@ class Namespace:
 
     @staticmethod
     def empty() -> 'Namespace':
+        """
+        Get an empty namespace.
+   *
+        @return An empty namespace
+        """
         return Namespace([])
 
     @staticmethod
     def of(*levels: str) -> 'Namespace':
+        """
+        Create a namespace with the given levels.
+   *
+        @param levels The levels of the namespace
+        @return A namespace with the given levels
+        """
         if levels is None:
             raise ValueError("Cannot create a namespace with null levels")
         if len(levels) == 0:
@@ -40,71 +48,163 @@ class Namespace:
 
     @staticmethod
     def of_metalake() -> 'Namespace':
+        """
+        Create a namespace for metalake.
+   *
+        @return A namespace for metalake
+        """
         return Namespace.empty()
 
     @staticmethod
     def of_catalog(metalake: str) -> 'Namespace':
+        """
+        Create a namespace for catalog.
+   *
+        @param metalake The metalake name
+        @return A namespace for catalog
+        """
         return Namespace.of(metalake)
 
     @staticmethod
     def of_schema(metalake: str, catalog: str) -> 'Namespace':
+        """
+        Create a namespace for schema.
+   *
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @return A namespace for schema
+        """
         return Namespace.of(metalake, catalog)
 
     @staticmethod
     def of_table(metalake: str, catalog: str, schema: str) -> 'Namespace':
+        """
+        Create a namespace for table.
+   *
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @param schema The schema name
+        @return A namespace for table
+        """
         return Namespace.of(metalake, catalog, schema)
 
     @staticmethod
     def of_fileset(metalake: str, catalog: str, schema: str) -> 'Namespace':
+        """
+        Create a namespace for fileset.
+   *
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @param schema The schema name
+        @return A namespace for fileset
+        """
         return Namespace.of(metalake, catalog, schema)
 
     @staticmethod
     def of_topic(metalake: str, catalog: str, schema: str) -> 'Namespace':
+        """
+        Create a namespace for topic.
+   *
+        @param metalake The metalake name
+        @param catalog The catalog name
+        @param schema The schema name
+        @return A namespace for topic
+        """
         return Namespace.of(metalake, catalog, schema)
 
     @staticmethod
     def check_metalake(namespace: 'Namespace') -> None:
+        """
+        Check if the given metalake namespace is legal, throw an IllegalNamespaceException if
+        it's illegal.
+   *
+        @param namespace The metalake namespace
+        """
         if not namespace and not namespace.is_empty():
             raise ValueError(f"Metalake namespace must be non-null and empty, the input namespace is {namespace}")
 
     @staticmethod
     def check_catalog(namespace: 'Namespace') -> None:
+        """
+
+        Check if the given catalog namespace is legal, throw an IllegalNamespaceException if
+        it's illegal.
+   *
+        @param namespace The catalog namespace
+        """
         if not namespace and namespace.length() != 1:
             raise ValueError(f"Catalog namespace must be non-null and have 1 level, the input namespace is {namespace}")
 
     @staticmethod
     def check_schema(namespace: 'Namespace') -> None:
+        """
+        Check if the given schema namespace is legal, throw an IllegalNamespaceException if
+        it's illegal.
+        @param namespace The schema namespace
+        """
         if not namespace and namespace.length() != 2:
             raise ValueError(f"Schema namespace must be non-null and have 2 levels, the input namespace is {namespace}")
 
     @staticmethod
     def check_table(namespace: 'Namespace') -> None:
+        """
+        Check if the given table namespace is legal, throw an IllegalNamespaceException if it's
+        illegal.
+        @param namespace The table namespace
+        """
         if not namespace and namespace.length() != 3:
             raise ValueError(f"Table namespace must be non-null and have 3 levels, the input namespace is {namespace}")
 
     @staticmethod
     def check_fileset(namespace: 'Namespace') -> None:
+        """
+        Check if the given fileset namespace is legal, throw an IllegalNamespaceException if
+        it's illegal.
+        @param namespace The fileset namespace
+        """
         if not namespace and namespace.length() != 3:
             raise ValueError(
                 f"Fileset namespace must be non-null and have 3 levels, the input namespace is {namespace}")
 
     @staticmethod
     def check_topic(namespace: 'Namespace') -> None:
+        """
+        Check if the given topic namespace is legal, throw an IllegalNamespaceException if it's
+        illegal.
+        @param namespace The topic namespace
+        """
         if not namespace and namespace.length() != 3:
             raise ValueError(f"Topic namespace must be non-null and have 3 levels, the input namespace is {namespace}")
 
     def levels(self) -> List[str]:
+        """
+        Get the levels of the namespace.
+        @return The levels of the namespace
+        """
         return self.levels
 
     def level(self, pos: int) -> str:
+        """
+        Get the level at the given position.
+        @param pos The position of the level
+        @return The level at the given position
+        """
         if pos < 0 or pos >= len(self.levels):
             raise ValueError("Invalid level position")
         return self.levels[pos]
 
     def length(self) -> int:
+        """
+        Get the length of the namespace.
+        @return The length of the namespace.
+        """
         return len(self.levels)
 
     def is_empty(self) -> bool:
+        """
+        Check if the namespace is empty.
+        @return True if the namespace is empty, false otherwise.
+        """
         return len(self.levels) == 0
 
     def __eq__(self, other: 'Namespace') -> bool:
@@ -120,5 +220,11 @@ class Namespace:
 
     @staticmethod
     def check(expression: bool, message: str, *args) -> None:
+        """
+        Check the given condition is true. Throw an IllegalNamespaceException if it's not.
+        @param expression The expression to check.
+        @param message The message to throw.
+        @param args The arguments to the message.
+        """
         if not expression:
             raise ValueError(message.format(*args))
